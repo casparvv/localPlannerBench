4583d26d37a2233199b6ac6538ee0a8fba3107b9
Caspar van Venrooij <casparvv52@gmail.com>
2022-10-07 16:00:53 +0200

    Add LiDAR visualization

--- plannerbenchmark/generic/experiment.py
+++ plannerbenchmark/generic/experiment.py
@@ -156,13 +156,18 @@ class Experiment(object):
         else:
             return gym.make(self.envName(), render=render, dt=self.dt())
 
+    def showLidar(self, env, sensor_data, q, body_ids_old, number_lidar_rays):
+        body_ids = env.show_lidar(sensor_data, q, body_ids_old, number_lidar_rays)
+        return body_ids
+    
     def addScene(self, env, nb_rays=0):
         # To do: make walls optional in the setup
         # Exp 1
         #env.add_walls(dim=np.array([0.2, 9.2, 0.5]), poses_2d=[[-4.5, 3.5, 0], [4.5, 3.5, 0], [0, -1, np.pi/2], [0, 8, np.pi/2]])
         # Exp 2
+        #env.add_walls(dim=np.array([0.2, 14, 0.5]), poses_2d=[[0, 3, np.pi/2]])
+        
         env.add_walls(dim=np.array([0.2, 2.2, 0.5]), poses_2d=[[0, -1, np.pi/2]])
-        #env.add_walls(dim=np.array([0.2, 2.2, 0.5]), poses_2d=[[0, 0.3, np.pi/2]])
         env.add_walls(dim=np.array([0.2, 10, 0.5]), poses_2d=[[-1, 4, 0]])
         env.add_walls(dim=np.array([0.2, 8, 0.5]), poses_2d=[[1, 3, 0]])
         env.add_walls(dim=np.array([0.2, 6.2, 0.5]), poses_2d=[[2, 9, np.pi/2]])
@@ -170,6 +175,7 @@ class Experiment(object):
         env.add_walls(dim=np.array([0.2, 6, 0.5]), poses_2d=[[5, 6, 0]])
         env.add_walls(dim=np.array([0.2, 4, 0.5]), poses_2d=[[3, 5, 0]])
         env.add_walls(dim=np.array([0.2, 2.2, 0.5]), poses_2d=[[4, 3, np.pi/2]])
+        
         for obst in self._obstacles:
             env.add_obstacle(obst)
         try:
